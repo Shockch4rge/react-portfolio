249e790a43c7077bc9d32cbeffcb9687f9426866
Shockch4rge <favouriteteo@gmail.com>
2022-04-11 02:16:05 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+const App = () => {
+	return (
+		<>
+			<div className="p-4 sm:p-2 flex flex-col container h-screen place-content-center">
+				<div>
+					<h1 className="text-4xl sm:text-7xl text-overtext text-left sm:text-center text-shadow-h1 m-auto">
+						Hello, I'm <span className="text-inherit font-bold">Favian.</span>
+					</h1>
+					<h3 className="mt-4 text-xl sm:text-3xl text-overtext text-left sm:text-center italic text-shadow-h1">
+						and I'm a...
+					</h3>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Arvo"],
+			},
+
+			colors: {
+				main: "#0F1C26",
+				overtext: "#9AF8FF",
+				undertext: "#A3DDE1",
+			},
+
+			textShadow: {
+				h1: "0 0.275rem rgba(163, 221, 225, 0.20)",
+				h3: "0 1px rgba(163, 221, 225, 0.20)",
+			},
+		},
+
+		container: {
+			center: true,
+		},
+	},
+	plugins: [require("tailwindcss-textshadow")],
+};
